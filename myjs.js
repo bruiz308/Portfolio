c51ecc1760baef485277525048bf438c5595e576
bruiz308 <40476533+bruiz308@users.noreply.github.com>
2018-07-18 12:56:48 -0700

    Add files via upload
    uploaded files from my portfolio

---
+++ myjs.js
@@ -0,0 +1,25 @@
+alert("hey you are awsome");
+
+var myButton= documnet.querySelector("button");
+var myHeading = document.querySelector("h1");
+
+myButton.onclick = function(){
+
+  var name = prompt("Enter Your Name");
+  myHeading.innerHTML() = name;
+
+}
+
+
+
+
+  //myheading.innerHTML
+
+
+
+
+// myButton.onclick = function(){
+//   var name = prompt('Please enter your name');
+//   localStorage,setItme('name: ',name);
+// }
+// link your js file to your HTML file.
